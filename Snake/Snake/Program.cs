658cb9edfb42dc5cdfb0984e5ff971789e32c7ea
ZvalsoraY <yazamail@yandex.ru>
2018-02-05 23:54:54 +0300

    ram

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,13 +10,37 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            Func1(x);
+            Console.WriteLine("x="+x);
+
+            Func2(x);
+            Console.WriteLine("Func2.x"+x);
+            
+
             Point p1 = new Point(1,3,'*');
             p1.Draw();
+            Move(p1, 10, 10);
+            p1.Draw();
+
 
             Point p2 = new Point(4,5,'#');
             p2.Draw();
             
             Console.ReadLine();
+        }
+        public static void Func1(int value)
+        {
+
+        }
+        public static void Func2(int value)
+        {
+            value = value + 1;
+        }
+        public static void Move(Point p,int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y + dy;
         }
     }
 }
