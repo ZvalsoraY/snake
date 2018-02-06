1048868a16be3185b8aafbefe7a5dbe8e21f8bd3
ZvalsoraY <yazamail@yandex.ru>
2018-02-06 22:54:27 +0300

    Add lines

---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        public List<Point> pListV;
+
+        public VerticalLine(int yL, int yH, int x, char sym)
+        {
+            pListV = new List<Point>();
+            for( int y= yL; y <= yH; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pListV.Add(p);
+
+            }
+            
+        }
+
+        public void Drow()
+        {
+            foreach(Point p in pListV)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
