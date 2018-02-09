a5855c4d337705cbc9eab04a3606305827bfb4b0
ZvalsoraY <yazamail@yandex.ru>
2018-02-09 23:49:59 +0300

    управление

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -21,20 +21,20 @@ namespace Snake
             leftLine.Drow();
             rightLine.Drow();
 
+            //отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey( key.Key );
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
 
             //Console.ReadLine();
         }
